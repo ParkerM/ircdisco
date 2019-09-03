75c6ae420acb9f2fc64d0cc4290b69d69d0b5963
Parker Mauney <parkermauney@gmail.com>
2019-09-02 19:04:02 -0500

    Add IRC and app config stuff, remove unused deps

--- src/main/java/io/github/parkerm/ircdisco/config/IrcConfiguration.java
+++ src/main/java/io/github/parkerm/ircdisco/config/IrcConfiguration.java
@@ -6,4 +6,38 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties(IrcProperties.class)
 public class IrcConfiguration {
+
+    private IrcProperties properties;
+
+    public IrcConfiguration(IrcProperties properties) {
+        this.properties = properties;
+    }
+
+    public String getNick() {
+        return properties.getNick();
+    }
+
+    public String getLogin() {
+        return properties.getLogin();
+    }
+
+    public boolean getAutoNickChange() {
+        return properties.isAutoNickChange();
+    }
+
+    public String getAutoJoinChannel() {
+        return properties.getAutoJoinChannel();
+    }
+
+    public String getServerUrl() {
+        return properties.getServerUrl();
+    }
+
+    public int getServerPort() {
+        return properties.getServerPort();
+    }
+
+    public boolean getSslEnabled() {
+        return properties.isSslEnabled();
+    }
 }
--- src/main/java/io/github/parkerm/ircdisco/config/IrcProperties.java
+++ src/main/java/io/github/parkerm/ircdisco/config/IrcProperties.java
@@ -1,7 +1,20 @@
 package io.github.parkerm.ircdisco.config;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("irc")
+@Data
+@Getter
+@Setter
 public class IrcProperties {
+    private String nick;
+    private String login;
+    private boolean autoNickChange;
+    private String autoJoinChannel;
+    private String serverUrl;
+    private int serverPort;
+    private boolean sslEnabled;
 }
